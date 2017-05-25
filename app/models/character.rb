41d6507e7b1ba0c7a25de43477095b54bfe5ef99
Garren Gotthardt <garren.x.gotthardt@gmail.com>
2017-05-25 18:00:04 -0400

    tvland

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,12 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to :actor
+  belongs_to :show
+
+  def say_that_thing_you_say
+    "Steve Urkel always says: #{self.catchphrase}"
+  end
+
+  def call_letters
+    self.call_letters
+  end
 end
